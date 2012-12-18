c2979d9411fcff4959daf38fae4c47e1587ec2c1
Olawale Ogunwale <wale.ogunwale@gmail.com>
2012-12-17 18:52:55 -0600

    Always get auth token when test activity starts.

--- picaface/src/com/ogunwale/android/app/picaface/PicasaAuth.java
+++ picaface/src/com/ogunwale/android/app/picaface/PicasaAuth.java
@@ -1,17 +1,11 @@
 package com.ogunwale.android.app.picaface;
 
-import java.io.IOException;
-
-import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.accounts.AccountManagerCallback;
 import android.accounts.AccountManagerFuture;
-import android.accounts.AuthenticatorException;
-import android.accounts.OperationCanceledException;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 
@@ -20,120 +14,78 @@ import com.google.api.client.googleapis.extensions.android2.auth.GoogleAccountMa
 /**
  * This class is responsible for authorization/authentication into the users
  * picasa web albums account.
- * 
+ *
  * @author ogunwale
- * 
+ *
  */
 public class PicasaAuth {
 
-	private static final String sTAG = PicasaAuth.class.getSimpleName();
+    // private static final String sTAG = PicasaAuth.class.getSimpleName();
 
-	/**
-	 * Picasa auth token type.
-	 */
-	private static final String PICASA_AUTH_TOKEN_TYPE = "lh2";
+    /**
+     * Picasa auth token type.
+     */
+    private static final String PICASA_AUTH_TOKEN_TYPE = "lh2";
 
-	/**
-	 * Current account name.
-	 */
-	private static String mAccountName = null;
+    /**
+     * Application context. mainly used to broadcast auth done action in
+     * callback.
+     */
+    private static Context mAppContext = null;
 
-	/**
-	 * Account authentication token.
-	 */
-	private static String mAuthToken = null;
+    /**
+     * All broadcasts sent by this class
+     *
+     * @author ogunwale
+     *
+     */
+    public class Broadcasts {
+        /**
+         * Class broadcast prefix
+         */
+        public static final String BROADCASTS_PREFIX = "PicasaAuth.Broadcasts.";
 
-	/**
-	 * Application context. mainly used to broadcast auth done action in
-	 * callback.
-	 */
-	private static Context mAppContext = null;
+        /**
+         * Action broadcasted we authentication is complete. Intent will also
+         * contain EXTRA_NAME_ACCOUNT_NAME and EXTRA_NAME_AUTHTOKEN extras.
+         */
+        public static final String ACTION_AUTHENTICATION_DONE = BROADCASTS_PREFIX + "ACTION_AUTHENTICATION_DONE";
 
-	/**
-	 * All broadcasts sent by this class
-	 * 
-	 * @author ogunwale
-	 * 
-	 */
-	public class Broadcasts {
-		/**
-		 * Action broadcasted we authentication is complete.
-		 */
-		public static final String ACTION_AUTHENTICATION_DONE = "PicasaAuth.Broadcasts.ACTION_AUTHENTICATION_DONE";
-	}
+        /**
+         * Authentication account name string.
+         */
+        public static final String EXTRA_NAME_ACCOUNT_NAME = BROADCASTS_PREFIX + "EXTRA_NAME_ACCOUNT_NAME";
+        /**
+         * Authentication token string.
+         */
+        public static final String EXTRA_NAME_AUTHTOKEN = BROADCASTS_PREFIX + "EXTRA_NAME_AUTHTOKEN";
+    }
 
-	public static void authenticate(Activity context) {
+    /**
+     * Asynchronous method call used to authenticate access to the picasa
+     * account. {@link Broadcasts.ACTION_AUTHENTICATION_DONE} will be
+     * broadcasted if the authentication was successful.
+     *
+     * @param context
+     */
+    public static void authenticate(Activity context) {
 
-		GoogleAccountManager am = new GoogleAccountManager(context);
+        mAppContext = context.getApplicationContext();
 
-		final SharedPreferences settings = context.getSharedPreferences(sTAG, Context.MODE_PRIVATE);
-		mAccountName = settings.getString("mAccountName", null);
-		mAuthToken = settings.getString("mAuthToken", null);
+        AccountManager.get(context).getAuthTokenByFeatures(GoogleAccountManager.ACCOUNT_TYPE, PICASA_AUTH_TOKEN_TYPE, null, context, null, null,
+                new AccountManagerCallback<Bundle>() {
+                    public void run(AccountManagerFuture<Bundle> future) {
+                        try {
+                            Bundle bundle = future.getResult();
 
-		Account account = am.getAccountByName(mAccountName);
+                            Intent intent = new Intent(Broadcasts.ACTION_AUTHENTICATION_DONE);
+                            intent.putExtra(Broadcasts.EXTRA_NAME_ACCOUNT_NAME, bundle.getString(AccountManager.KEY_ACCOUNT_NAME));
+                            intent.putExtra(Broadcasts.EXTRA_NAME_AUTHTOKEN, bundle.getString(AccountManager.KEY_AUTHTOKEN));
 
-		if (account == null) {
-			mAppContext = context.getApplicationContext();
-
-			AccountManager.get(context).getAuthTokenByFeatures(GoogleAccountManager.ACCOUNT_TYPE, PICASA_AUTH_TOKEN_TYPE, null, context, null, null,
-					new AccountManagerCallback<Bundle>() {
-						public void run(AccountManagerFuture<Bundle> future) {
-							try {
-								Bundle bundle = future.getResult();
-								mAccountName = bundle.getString(AccountManager.KEY_ACCOUNT_NAME);
-								mAuthToken = bundle.getString(AccountManager.KEY_AUTHTOKEN);
-
-								settings.edit().putString("mAccountName", mAccountName);
-								settings.edit().putString("mAuthToken", mAuthToken);
-								LocalBroadcastManager.getInstance(mAppContext).sendBroadcast(new Intent(Broadcasts.ACTION_AUTHENTICATION_DONE));
-							} catch (Exception e) {
-							}
-						}
-					}, null);
-		} else {
-			LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(Broadcasts.ACTION_AUTHENTICATION_DONE));
-		}
-	}
-
-	/**
-	 * Method is used to refresh the auth token when ever it expires.
-	 * 
-	 */
-	public static void refreshAuthToken(Context context) {
-		AccountManager am = AccountManager.get(context);
-		am.invalidateAuthToken(GoogleAccountManager.ACCOUNT_TYPE, mAuthToken);
-
-		Account account = new Account(mAccountName, GoogleAccountManager.ACCOUNT_TYPE);
-
-		try {
-			mAuthToken = am.blockingGetAuthToken(account, GoogleAccountManager.ACCOUNT_TYPE, false);
-		} catch (OperationCanceledException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (AuthenticatorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Returns the current picasa user account name or null if not valid
-	 * 
-	 * @return current picasa account name or null
-	 */
-	public static String getAccountName() {
-		return mAccountName;
-	}
-
-	/**
-	 * Returns the current picasa auth token or null if not valid
-	 * 
-	 * @return current picasa auth token
-	 */
-	public static String getAuthToken() {
-		return mAuthToken;
-	}
+                            LocalBroadcastManager.getInstance(mAppContext).sendBroadcast(intent);
+                        } catch (Exception e) {
+                        }
+                    }
+                }, null);
+    }
 }
--- picaface/src/com/ogunwale/android/app/picaface/TestActivity.java
+++ picaface/src/com/ogunwale/android/app/picaface/TestActivity.java
@@ -28,119 +28,135 @@ import android.view.Menu;
 
 public class TestActivity extends Activity {
 
-	private static final String sTAG = TestActivity.class.getSimpleName();
+    private static final String sTAG = TestActivity.class.getSimpleName();
 
-	private static final String APP_NAME = "picaface";
+    private static final String APP_NAME = "picaface";
 
-	private class GetAlbumsTask extends TimerTask {
-		@Override
-		public void run() {
-			PicasaAuth.refreshAuthToken(getApplicationContext());
-			HttpTransport transport = new NetHttpTransport();
-			HttpRequestFactory factory = transport.createRequestFactory(new HttpRequestInitializer() {
-				@Override
-				public void initialize(HttpRequest request) throws IOException {
-					GoogleHeaders headers = new GoogleHeaders();
-					headers.setApplicationName(APP_NAME);
-					headers.setGDataVersion("2");
-					headers.setGoogleLogin(PicasaAuth.getAuthToken());
-					request.setHeaders(headers);
-				}
-			});
+    private String mAuthToken = null;
+    private String mAccountName = null;
 
-			PicasaClient picasaClient = new PicasaClient(factory);
-			picasaClient.setApplicationName(APP_NAME);
+    private class GetAlbumsTask extends TimerTask {
+        @Override
+        public void run() {
+            HttpTransport transport = new NetHttpTransport();
+            HttpRequestFactory factory = transport.createRequestFactory(new HttpRequestInitializer() {
+                @Override
+                public void initialize(HttpRequest request) throws IOException {
+                    GoogleHeaders headers = new GoogleHeaders();
+                    headers.setApplicationName(APP_NAME);
+                    headers.setGDataVersion("2");
+                    headers.setGoogleLogin(mAuthToken);
+                    request.setHeaders(headers);
+                }
+            });
 
-			// build URL for the default user feed of albums
-			PicasaUrl url = PicasaUrl.relativeToRoot("feed/api/user/default");
-			// execute GData request for the feed
-			UserFeed feed = null;
-			try {
-				feed = picasaClient.executeGetUserFeed(url);
-				Log.i(sTAG, "User: " + feed.author.name);
-				Log.i(sTAG, "Total number of albums: " + feed.totalResults);
-				// show albums
-				if (feed.albums != null) {
-					for (AlbumEntry album : feed.albums) {
-						Log.i(sTAG, "-----------------------------------------------");
-						Log.i(sTAG, "Album title: " + album.title);
-						Log.i(sTAG, "Updated: " + album.updated);
-						Log.i(sTAG, "Album ETag: " + album.etag);
-						if (album.summary != null) {
-							Log.i(sTAG, "Description: " + album.summary);
-						}
-						// if (album.numPhotos != 0) {
-						// Log.i("Total number of photos: " + album.numPhotos);
-						// PicasaUrl url = new PicasaUrl(album.getFeedLink());
-						// AlbumFeed feed = client.executeGetAlbumFeed(url);
-						// for (PhotoEntry photo : feed.photos) {
-						// Log.i();
-						// Log.i("Photo title: " + photo.title);
-						// if (photo.summary != null) {
-						// Log.i("Photo description: " + photo.summary);
-						// }
-						// Log.i("Image MIME type: " +
-						// photo.mediaGroup.content.type);
-						// Log.i("Image URL: " + photo.mediaGroup.content.url);
-						// }
-					}
-				}
-			} catch (HttpResponseException e) {
-				int code = e.getStatusCode();
-				if (code == 401 /* Unauthorized */|| code == 403 /*
-																 * forebidden
-																 * token
-																 * expired or invalid.
-																 */) {
-				}
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+            PicasaClient picasaClient = new PicasaClient(factory);
+            picasaClient.setApplicationName(APP_NAME);
 
-	private LocalBroadcastReciever mLocalBroadcastReciever = new LocalBroadcastReciever();
+            // build URL for the default user feed of albums
+            PicasaUrl url = PicasaUrl.relativeToRoot("feed/api/user/default");
+            // execute GData request for the feed
+            UserFeed feed = null;
+            try {
+                String nextLink = null;
+                feed = picasaClient.executeGetUserFeed(url);
+                do {
+                    Log.i(sTAG, "User: " + feed.author.name);
+                    Log.i(sTAG, "Total number of albums: " + feed.totalResults);
+                    // show albums
+                    if (feed.albums != null) {
+                        for (AlbumEntry album : feed.albums) {
+                            Log.i(sTAG, "-----------------------------------------------");
+                            Log.i(sTAG, "Album title: " + album.title);
+                            Log.i(sTAG, "Updated: " + album.updated);
+                            Log.i(sTAG, "Album ETag: " + album.etag);
+                            if (album.summary != null) {
+                                Log.i(sTAG, "Description: " + album.summary);
+                            }
+                            // if (album.numPhotos != 0) {
+                            // Log.i("Total number of photos: " +
+                            // album.numPhotos);
+                            // PicasaUrl url = new
+                            // PicasaUrl(album.getFeedLink());
+                            // AlbumFeed feed = client.executeGetAlbumFeed(url);
+                            // for (PhotoEntry photo : feed.photos) {
+                            // Log.i();
+                            // Log.i("Photo title: " + photo.title);
+                            // if (photo.summary != null) {
+                            // Log.i("Photo description: " + photo.summary);
+                            // }
+                            // Log.i("Image MIME type: " +
+                            // photo.mediaGroup.content.type);
+                            // Log.i("Image URL: " +
+                            // photo.mediaGroup.content.url);
+                            // }
+                        }
+                    }
+                    nextLink = feed.getNextLink();
+                    if(nextLink != null)
+                        feed = picasaClient.executeGetUserFeed(new PicasaUrl(nextLink));
+                } while (nextLink != null);
+            } catch (HttpResponseException e) {
+                int code = e.getStatusCode();
+                if (code == 401 /* Unauthorized */|| code == 403 /*
+                                                                  * forebidden
+                                                                  * token
+                                                                  * expired or
+                                                                  * invalid.
+                                                                  */) {
+                }
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
-	private class LocalBroadcastReciever extends BroadcastReceiver {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
+    private LocalBroadcastReciever mLocalBroadcastReciever = new LocalBroadcastReciever();
 
-			if (PicasaAuth.Broadcasts.ACTION_AUTHENTICATION_DONE.equals(action)) {
-				GetAlbumsTask task = new GetAlbumsTask();
-				Timer timer = new Timer();
-				timer.schedule(task, 0);
-			}
-		}
+    private class LocalBroadcastReciever extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
 
-	}
+            if (PicasaAuth.Broadcasts.ACTION_AUTHENTICATION_DONE.equals(action)) {
+                mAuthToken = intent.getStringExtra(PicasaAuth.Broadcasts.EXTRA_NAME_AUTHTOKEN);
+                mAccountName = intent.getStringExtra(PicasaAuth.Broadcasts.EXTRA_NAME_ACCOUNT_NAME);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_test);
+                if (mAuthToken != null && mAccountName != null) {
+                    GetAlbumsTask task = new GetAlbumsTask();
+                    Timer timer = new Timer();
+                    timer.schedule(task, 0);
+                }
+            }
+        }
+    }
 
-		// Register for authentication done broadcast.
-		IntentFilter filter = new IntentFilter(PicasaAuth.Broadcasts.ACTION_AUTHENTICATION_DONE);
-		LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mLocalBroadcastReciever, filter);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
 
-		// Start authentication process if needed.
-		PicasaAuth.authenticate(this);
+        // Register for authentication done broadcast.
+        IntentFilter filter = new IntentFilter(PicasaAuth.Broadcasts.ACTION_AUTHENTICATION_DONE);
+        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(mLocalBroadcastReciever, filter);
 
-	}
+        // Start authentication process if needed.
+        PicasaAuth.authenticate(this);
 
-	protected void onDestory() {
-		super.onDestroy();
-		LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(mLocalBroadcastReciever);
-	}
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_test, menu);
-		return true;
-	}
+    protected void onDestory() {
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(mLocalBroadcastReciever);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_test, menu);
+        return true;
+    }
 
 }
